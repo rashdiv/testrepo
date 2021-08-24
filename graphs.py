ba9ec8a05ce0d1974bc8f2c99bebc3dd3ca952dd
rashdiv <89475099+rashdiv@users.noreply.github.com>
2021-08-24 22:22:58 +0400

    Add files via upload

---
+++ graphs.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 20 05:37:26 2021
+
+@author: rashmi.dwivedi
+"""
+
+import matplotlib.pyplot as plt
+
+plt.plot([1,2,3,4],[1,4,9,16],'ro')
+plt.axis(0,6,0,20)
+plt.show()
+
+plt.plot([1,2,3,4],[1,4,9,16],'o-g')
+plt.axis(0,6,0,20)
+plt.show()
